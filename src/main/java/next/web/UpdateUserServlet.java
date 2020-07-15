a64e66cb612798b8e50ce012b90f85e8c8f23632
Jaehee Park <jaeheepark0145@gmail.com>
2020-07-15 17:31:48 +0900

    UpdateUserServlet 생성

---
+++ src/main/java/next/web/UpdateUserServlet.java
@@ -0,0 +1,8 @@
+package next.web;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/user/update")
+public class UpdateUserServlet extends HttpServlet {
+}
